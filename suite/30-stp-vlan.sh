3a31d4e514670b24d94ed373123993d3b8ea667c
Joachim Wiberg <troglobit@gmail.com>
2021-09-02 15:11:45 +0200

    suite: skip test if STP per VLAN is not supported
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- suite/30-stp-vlan.sh
+++ suite/30-stp-vlan.sh
@@ -6,6 +6,10 @@ basic_stp_vlan()
     require3loops
 
     create_br $br0 "vlan_filtering 1" $xports
+    if ! bridge -d vlan show | grep -q "state forwarding"; then
+        step "STP per VLAN not supported, skipping."
+        skip
+    fi
 
     bridge vlan add vid 2 dev $bx pvid untagged
     bridge vlan del vid 1 dev $bx
@@ -25,6 +29,7 @@ basic_stp_vlan()
 
     step "Setting VLAN 1 on $cx in blocking state"
     bridge vlan set vid 1 dev $cx state blocking
+    bridge -d vlan show
 
     step "Injecting broadcast on $ax and $bx"
     capture $cy
