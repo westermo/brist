c6cd1e793ce1b7b552510a855fadc3bc5d92099f
Tobias Waldekranz <tobias@waldekranz.com>
2022-02-22 14:33:04 +0100

    suite/stp-vlan: Add MSTP test

--- suite/30-stp-vlan.sh
+++ suite/30-stp-vlan.sh
@@ -47,3 +47,84 @@ basic_stp_vlan()
     pass
 }
 experimentaltests="$experimentaltests basic_stp_vlan"
+
+mstp_verify()
+{
+    capture $h2
+
+    step "  Injecting broadcast in VLAN 10,20,30,40, on $h1"
+    for vid in 10 20 30 40; do
+	eth -b -q $vid | { cat; echo from $vid; } | inject $h1
+    done
+
+    for vid in 10 20 30 40; do
+	if echo "$@" | grep -q $vid; then
+	    step "  Verifying broadcast in VLAN $vid, on $h2"
+	    report $h2 | grep -q "from $vid" || return 1
+	else
+	    step "  Verifying absense of broadcast in VLAN $vid, on $h2"
+	    report $h2 | grep -q "from $vid" && return 1
+	fi
+    done
+
+    return 0
+}
+
+basic_mstp()
+{
+    require2loops
+
+    if ! bridge vlan help 2>&1 | grep -q msti; then
+        step "Mapping of VLANs to MSTIs not supported, skipping."
+        skip
+    fi
+
+    vlanopts="vlan_filtering 1 vlan_default_pvid 0"
+    stpopts="stp_state 2 mst_enable 1"
+
+    create_br $br0 "$vlanopts $stpopts" $bports
+
+    for bport in $b1 $b2; do
+	for vid in 10 20 30 40; do
+	    bridge vlan add vid $vid dev $bport
+	done
+    done
+
+    bridge link set dev $b2 state forwarding
+
+    step "All VLANs should start out in CST, which should be blocking"
+    mstp_verify || fail
+
+    step "Setting CST(all VLANs) on $b1 in forwarding state"
+    bridge link set dev $b1 state forwarding
+    mstp_verify 10 20 30 40 || fail
+
+    step "Moving VLAN 20 and 30 to MSTI 100, should start out disabled"
+    bridge vlan global set vid 20 dev $br0 msti 100
+    bridge vlan global set vid 30 dev $br0 msti 100
+    bridge mst set dev $b2 msti 100 state forwarding
+    mstp_verify 10 40 || fail
+
+    step "Setting MSTI 100 on $b1 in forwarding state"
+    bridge mst set dev $b1 msti 100 state forwarding
+    mstp_verify 10 20 30 40 || fail
+
+    step "Setting MSTI 100 on $b1 in blocking state"
+    bridge mst set dev $b1 msti 100 state blocking
+    mstp_verify 10 40 || fail
+
+    step "Moving VLAN 40 to MSTI 100, should inherit state"
+    bridge vlan global set vid 40 dev $br0 msti 100
+    mstp_verify 10 || fail
+
+    step "Setting MSTI 100 on $b1 in forwarding state"
+    bridge mst set dev $b1 msti 100 state forwarding
+    mstp_verify 10 20 30 40 || fail
+
+    step "Setting CST(VLAN 10) on $b1 in blocking state"
+    bridge link set dev $b1 state blocking
+    mstp_verify 20 30 40 || fail
+
+    pass
+}
+experimentaltests="$experimentaltests basic_mstp"
